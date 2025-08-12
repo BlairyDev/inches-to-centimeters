81672fc570daf10ef09febdc95d2930c0e7a9325
162504966+BlairyDev@users.noreply.github.com <162504966+FlowerKnight55@users.noreply.github.com>
2025-08-12 14:02:17 -0400

    Finished

---
+++ app/src/main/java/com/example/inchestocentimeters/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.inchestocentimeters
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private val inchValue = 2.54
+    private lateinit var enterInches: EditText
+    private lateinit var convertButton: Button
+    private lateinit var textViewCentimeters: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        enterInches = findViewById(R.id.editTextInches)
+        convertButton = findViewById(R.id.button_convert)
+        textViewCentimeters = findViewById(R.id.textViewConvert)
+
+        convertButton.setOnClickListener {
+            if(!enterInches.text.isEmpty()) {
+                val result = enterInches.text.toString().toDouble() * inchValue
+                textViewCentimeters.text = result.toString()
+            } else {
+                textViewCentimeters.text = getString(R.string.text)
+            }
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
